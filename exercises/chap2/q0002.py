2a3cb6e34b2210bf52a5f3e719a887b90624b008
masataka kanno <akapanna@gmail.com>
2013-07-03 17:34:46 +0900

    answered q0002

--- exercises/chap2/q0002.py
+++ exercises/chap2/q0002.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python
 
 def fizzbuff(n):
+    """ Return 'FizzBuzz' String
 
-    #place your implementation here
-    pass
+    Arguments:
+        n - some number
 
-
+    Returns: string
+        If it is a divisor of 3  - Fizz
+        If it is a divisor of 5  - Buzz
+        If it is a divisor of 15 - Fizz Buzz
+    """
+    if n % 15 == 0:
+        return 'Fizz Buzz'
+    elif n % 3 == 0:
+        return 'Fizz'
+    elif n % 5 == 0:
+        return 'Buzz'
+    return n
