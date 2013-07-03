0d6fe8d8a5f355a13a4086f6e6510cdf290d0ffc
masataka kanno <akapanna@gmail.com>
2013-07-03 18:27:44 +0900

    answered q0007

--- exercises/chap2/q0007.py
+++ exercises/chap2/q0007.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python
 
 def wordcount(xs):
+    """ Count each word count
 
-    #place your implementation here
-    pass
+    Arguments:
+        xs - List
+
+    Returns: Dict
+        key   - word
+        value - count
+    """
+    counts = {}
+    for x in xs:
+        count = counts[x] if x in counts else 0
+        counts[x] = count + 1
+    return counts
