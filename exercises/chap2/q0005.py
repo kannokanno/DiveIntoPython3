87f1e72f090daa1456b3756659e6d8ac7715aa9c
masataka kanno <akapanna@gmail.com>
2013-07-03 18:03:23 +0900

    answerd q0005

--- exercises/chap2/q0005.py
+++ exercises/chap2/q0005.py
@@ -1,7 +1,23 @@
-
+# -*- coding: utf-8 -*-
 #!/usr/bin/python
 
 def numbering(xs):
+    """ Convert xs to [(index, element), ...]
 
-    #place your implementation here
-    pass
+    Arguments:
+        xs - List
+
+    Returns: List of tuple
+        (index, element)
+        index   - index of element
+        element - original element
+    """
+    # 手続き的。素直
+    number_pairs = []
+    for i in range(len(xs)):
+        number_pairs.append((i, xs[i]))
+    return number_pairs
+
+# 別解。ただこれだと練習にならないかもしれない。
+def _numbering(xs):
+    return list(enumerate(xs))
