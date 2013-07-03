767618e65749f27a4ff07ed92944ab3d97a6d1cc
masataka kanno <akapanna@gmail.com>
2013-07-03 18:34:11 +0900

    answered q0008

--- exercises/chap2/q0008.py
+++ exercises/chap2/q0008.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python
 
 def flatten(xs):
+    """ Flatten of list
 
-    #place your implementation here
-    pass
+    Arguments:
+        xs - List
+
+    Returns: List
+    """
+    return _flatten(xs, [])
+
+def _flatten(xs, result):
+    for x in xs:
+        if isinstance(x, list):
+            _flatten(x, result)
+        else:
+            result.append(x)
+    return result
