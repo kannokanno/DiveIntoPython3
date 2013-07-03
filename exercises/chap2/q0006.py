86b8a0d7c59c55916e45a751c720d4c6cfd3d151
masataka kanno <akapanna@gmail.com>
2013-07-03 18:23:59 +0900

    answered q0006

--- exercises/chap2/q0006.py
+++ exercises/chap2/q0006.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python
 
 def false_only(xs):
+    """ Select only false elements
 
-    #place your implementation here
-    pass
+    Arguments:
+        xs - List
+
+    Returns: List
+    """
+    # 内包表記を使えばもっと簡単だけどchap2では出てこないので使わない
+    false_elements = []
+    for x in xs:
+        if not xs:
+            false_elements.append(x)
+    return false_elements
